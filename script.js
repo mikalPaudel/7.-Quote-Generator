39b8ec51fc118d92fd1eecae875e4068e7bf2e4e
Mikal <mikalpaudel58@gmail.com>
2025-09-17 01:38:11 +0545

    Update quote display functionality and adjust container width

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const api_url = "https://api.allorigins.win/get?url=https://zenquotes.io/api/random";
 
+const quote = document.querySelector("#quote-display");
+const quoteAuthor = document.querySelector("#author");
 async function getQuote(url) {
-    const response = await fetch(url);
+    const response = await fetch(url + "&timestamp=" + Date.now());
     const data = await response.json();
     console.log(data);
+    quote.innerHTML = JSON.parse(data.contents)[0].q;
+    quoteAuthor.innerHTML = "- " + JSON.parse(data.contents)[0].a;
 }
 getQuote(api_url);
+
 
 
 
